96dc7bf5fdf5b0933004bafaabcef8d9169ba85d
Ezra Crocker <Rage300burger@gmail.com>
2026-02-09 23:03:04 -0500

    increased robustness of memsize syscall checker

--- procmemsize.c
+++ procmemsize.c
@@ -7,5 +7,16 @@ main(int argc, char *argv[])
 {
   printf(1, "proc mem size: %d\n", get_mem_size());
 
+  printf(1, "malloc 10kb\n");
+
+  char *ptr = malloc(1024*10);
+  
+  printf(1, "proc mem size: %d\n", get_mem_size());
+
+  printf(1, "free ptr\n");
+  
+  free(ptr);
+  
+  printf(1, "proc mem size: %d\n", get_mem_size());
   exit();
 }
